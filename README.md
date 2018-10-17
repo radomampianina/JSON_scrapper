bcf3e55f932d850a3c59cee0fe0457c1cc228b9d
radomampianina <randriampenitra@yahoo.com>
2018-10-17 23:00:34 +0300

    spreadsheet Goodle Driver ok

---
+++ README.md
@@ -0,0 +1,2 @@
+# extractraction site et envoi vers google_drive
+Commenter les fonctions que vous n'utiliser pas pour eviter une longue attente au upload
--- app.rb
+++ app.rb
@@ -5,13 +5,29 @@ require 'nokogiri'
 require 'json'
 
 $:.unshift File.expand_path("./../lib", __FILE__)
-# require 'lib/scrapper'
 require 'scrapper'
+# extractraction villes et email
 Ville_email.new.emails('val-d-oise')
 
 # mamorona json
-hash_to_json = Ville_email.new.show_mail
-File.open("db/emails.JSON","w") do |f|
+ hash_to_json = Ville_email.new.show_mail
+ File.open("db/emails.JSON","w") do |f|
   f.write(hash_to_json.to_json)
-end
-# session = GoogleDrive::Session.from_config("config.json")
+ end
+
+# https://docs.google.com/spreadsheets/d/1F24Jy-3xOmO4UxL6aK_CtswGxyTowbfS_Z8PbH8Srw0/edit#gid=0
+  session = GoogleDrive::Session.from_config("config.json")
+  ws = session.spreadsheet_by_key("1F24Jy-3xOmO4UxL6aK_CtswGxyTowbfS_Z8PbH8Srw0").worksheets[0]
+ # envoi json vers spreadsheets
+ fJson = File.open("db/emails.JSON","r")
+ data = fJson.read
+  fJson.close
+ my_hash = JSON.parse(data)
+ st = 1
+ while my_hash.keys[st].is_a? String
+   ws[st, 1] = my_hash.keys[st]
+   ws[st, 2] = my_hash.values[st]
+   st += 1
+ end
+ ws.save
+# voir https://docs.google.com/spreadsheets/d/1F24Jy-3xOmO4UxL6aK_CtswGxyTowbfS_Z8PbH8Srw0/
